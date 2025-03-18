28b6a8ae6dab771aecf7f0afa73520fdf070cd3b
MnsDew <mnssory9999@gmail.com>
2025-03-18 22:06:32 +0300

    feat: New Portfolio nodeJS and ReactJS in same repo

---
+++ portfolio/src/components/Gallery.js
@@ -0,0 +1,28 @@
+ import React from "react";
+ import IMAGES from "../data/images";
+ import ImageShadow from "react-image-shadow";
+ import "react-image-shadow/assets/index.css";
+
+ const Gallery = () => {
+    return(
+        <div> 
+       {IMAGES.map(imageObject => { //we are moving through the map function
+        return <Image src={imageObject.image} desc={imageObject.description}/>
+       })}
+        </div>
+    )
+ }
+ const Image = ({src , desc}) => { // we need to get them as input parameters , deconstruct way 
+    // or inside (props) and down write src,desc WAY2 
+    // we use a or <a/> element to be clickable 
+    // and we get the paramaters from the other component 
+    return (
+        <div className="gallery">
+     <a target="_blank" rel="" href={src}>
+        <ImageShadow src={src}/></a>
+        <p>{desc}</p>
+        </div>
+    )
+ }
+
+ export default Gallery;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav-container">
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/amthal">Quotes</Link>
+        <Link className="nav-link" to="/Gallery">Gallery</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
