8c515dfe50dd37c056e98c5e5f65e47a9ec5b507
MnsDew <mnssory9999@gmail.com>
2025-03-25 07:35:56 +0300

    Update: new functionality in Quotes page

--- portfolio/src/components/Amthal.js
+++ portfolio/src/components/Amthal.js
@@ -1,5 +1,5 @@
 import React , {Component} from "react";
-
+import '../Amthal.css';
 class Amthal extends Component{
 constructor (){
     super();
@@ -21,25 +21,44 @@ constructor (){
         .then((json) => this.setState({TenQuotes: json}));
          
     }
-     
-    render(){
-        return(
-            <div style={{margin:50}}>
-            <h2>Today's Quote</h2>
-            <p>{this.state.mathal.title}</p>
-            <hr></hr>
-            <h3 style={{margin:30}}>Looking for more quotes?</h3>
-            <button style={{margin:50}} className="btn" onClick={this.fetchAmthal}>More...Quotes </button>
-           
-            { 
-                this.state.TenQuotes.map(mathal => {
-                    return <p key={mathal.id}>{mathal.title}</p>
-                })
-            }
-            </div>
-        )
-    }
 
+    render() {
+        return (
+            <div className="amthal-container">
+                <h2>Today's Quote</h2>
+                <div className="quote-card">{this.state.mathal.title}</div>
+                <hr />
+                <h3 className="amthal-title">Find Your Next Favorite Quote</h3>
+                <button className="amthal-button" onClick={this.fetchAmthal}>More...Quotes</button>
+
+                {this.state.TenQuotes.map((mathal) => {
+                    return <div key={mathal.id} className="quote-card">{mathal.title}</div>;
+                })}
+            </div>
+        );
+    }
 }
+
+     
+//     render(){
+//         return(
+//             <div style={{margin:50}}>
+//             <h2>Today's Quote</h2>
+//             <p>{this.state.mathal.title}</p>
+//             <hr></hr>
+//             <h3 style={{margin:30}}>Looking for more quotes?</h3>
+//             <button style={{margin:50}} className="btn" onClick={this.fetchAmthal}>More...Quotes </button>
+
+           
+//             { 
+//                 this.state.TenQuotes.map(mathal => {
+//                     return <p key={mathal.id}>{mathal.title}</p>
+//                 })
+//             }
+//             </div>
+//         )
+//     }
+
+// }
 
 export default Amthal;
